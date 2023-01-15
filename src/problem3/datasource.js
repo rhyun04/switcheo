b4c32a9a6951592b1d348dd6dba63b34013da326
rhyun04 <rhys.tan.2020@scis.smu.edu.sg>
2023-01-16 00:16:20 +0800

    q3

--- src/problem3/datasource.js
+++ src/problem3/datasource.js
@@ -1,1 +1,31 @@
 // your code here:
+
+// fetch('https://interview.switcheo.com/test.json')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
+// fetch('https://interview.switcheo.com/test.json', {
+//     mode: 'no-cors',
+//     method: 'GET',
+// })
+// .then(response => response.json())
+// .then(response => console.log(JSON.stringify(response)))
+// .catch(error =>{
+//     console.log("error");
+// })
+
+
+// fetch('https://interview.switcheo.com/test.json', {
+//     method: 'GET',
+//     mode: 'no-cors',
+//     headers: {
+//         'Accept': 'application/json',
+//     },
+// })
+// .then(response => response.json())
+// .then(response => console.log(JSON.stringify(response)))
+
+
+class ds {
+    
+}
